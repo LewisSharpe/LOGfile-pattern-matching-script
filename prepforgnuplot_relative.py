c91204d3783a26fabb0dbbdb1d0d0e1486e1f555
LewisSharpe <lewissharpe93@gmail.com>
2020-05-18 21:44:00 +0100

    python script calculates speed up through user input of core number using format 'core-number-64' for example. csv file write now working. working on correctly inputting the data correctly into gnuplot for the correct diagrams.

---
+++ prepforgnuplot_relative.py
@@ -0,0 +1,17 @@
+# ----------------------------------------------------------------------------------$
+# Python script that preps file with speedup times ready for gnuplot
+# Lewis Sharpe
+# Date: 18/05/20
+# Run with command: python prepforgnuplot_relative.py
+# ----------------------------------------------------------------------------------$
+infile = "relative.txt"
+outfile = "relative_cleaned.txt"
+delete_list = ["core-number-"]
+fin = open(infile)
+fout = open(outfile, "w+")
+for line in fin:
+    for word in delete_list:
+        line = line.replace(word, "")
+    fout.write(line)
+fin.close()
+fout.close()
